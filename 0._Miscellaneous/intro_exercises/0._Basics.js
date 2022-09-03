ec0e4dd58804d097b98cb4e301dc12c76b0cf89a
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2022-09-03 15:43:11 +0200

    Første opgave løst

--- 0._Miscellaneous/intro_exercises/0._Basics.js
+++ 0._Miscellaneous/intro_exercises/0._Basics.js
@@ -10,18 +10,8 @@ const lastName = "Latif";
 // Print out the following in the console:
 // My first name is Anders and my last name is Latif
 
-// const message = "My first name is " + firstName + " and my last name is " + lastName;
-// console.log(message);
-
-// console.log("My first name is", firstName, "and my last name is", lastName);
-
-// const message = `My first name is ${firstName} and my last name is ${lastName}`;
-// console.log(message);
-
-
-const messageVersionOne = "It's so easy.";
-const messageVersionTwo = 'I can write " easily.';
-const messageVersionThree = `'''"""''''"""`;
+const message = "My first name is " + firstName + " and my last name is " + lastName
+console.log(message);
 
 
 // --------------------------------------
@@ -34,16 +24,9 @@ const number = 1;
 // The result should be 2022
 // You cannot touch line 1 or 2
 
-// const result = parseInt(year) + number;
-// const result = Number(year) + number;
+const result = parseInt(year) + number;
 
-// const result = ~~year + number;
-// const result = +year + number;
-
-// console.log(result);
-
-console.log(parseInt("32432432fsdfdsf332432dsf"));
-console.log(Number("32432432fsdfdsf332432dsf"));
+console.log(result);
 
 // --------------------------------------
 
